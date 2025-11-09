8c190fbac8bc738c4387a79a6c85553cff05d581
Louis Hatté <louishatte@gmail.com>
2025-11-10 00:02:30 +0100

    v1.0.20 Fix years of experience: 5 to 4

--- src/components/footer/Footer.svelte
+++ src/components/footer/Footer.svelte
@@ -6,7 +6,7 @@
   <div class="copyright">
     © Copyright {new Date().getFullYear()}. {$_("footer-copyright")}
   </div>
-  <div class="version">Version - 1.0.19</div>
+  <div class="version">Version - 1.0.20</div>
 </div>
 
 <style lang="scss">
